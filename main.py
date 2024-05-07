45c84e960e84f4322e9e3233543bec4b1a916eec
samiulhuda360 <samidurbar@gmail.com>
2024-05-07 17:08:51 +0600

    port setting

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ async def scrape_endpoint():
         if not results:
             # If no tasks started, yield a ping event to keep connection alive
             yield "event: ping\ndata: null\n\n".encode()
+            yield "event: complete\ndata: null\n\n".encode()
             return
 
         while True:
@@ -110,7 +111,7 @@ async def scrape_endpoint():
             else:
                 # If no results are available, check if all tasks are completed
                 if all(result.ready() for result in results):
-                    # If all tasks are completed, send the "complete" event
+                    # If all tasks are completed, send the "complete" event and break the loop
                     complete_event = "event: complete\ndata: null\n\n"
                     yield complete_event.encode()
                     break
